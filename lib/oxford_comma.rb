b0098bdb00aa2880cd209b7a7e035f47233982c4
Dalton Fant <daltonfant16@gmail.com>
2018-02-07 00:32:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,12 @@
 def oxford_comma(array)
   if 1
-    array.join 
+    array.join
   elsif 2
     array.join(" and ")
-  else 
+  else
     array.join(", ") << ", and #{array[-1]}"
   end
-    
-    
+
+
 
 end
